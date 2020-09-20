876f852e47cd3373ef1648d2b5c1d4a81c77c4e2
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-09-20 21:21:30 +0800

    Digital support allowed

--- DigitalTest/DigitalTest.ino
+++ DigitalTest/DigitalTest.ino
@@ -1,4 +1,4 @@
-const int outPin = 3;
+const int outPin = 4;
 int inPin = 2;
 int value;
 void setup()
@@ -6,7 +6,7 @@ void setup()
   Serial.begin(9600);
   pinMode(inPin, INPUT);
   pinMode(outPin, OUTPUT);
-  Serial.println("Reading at pin 2, writing at pin 3. ");
+  Serial.println("Reading at pin 2, writing at pin 4. ");
   Serial.println("Serial input to change reading pin. ");
 }
 void loop()
@@ -16,7 +16,7 @@ void loop()
     inPin = Serial.read();
     Serial.println("Reading pin change to " + inPin);
   }
-  value = analogRead(inPin);
+  value = digitalRead(inPin);
   Serial.println(value);
-  analogWrite(outPin, value);
+  digitalWrite(outPin, value);
 }
